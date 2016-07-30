0b3ab410b83aca4211d07c3636d13d6ab9c0c782
Joshua T. Fisher <playmer@gmail.com>
2016-07-30 15:00:17 -0700

    Moved some functions around.

--- User.c
+++ User.c
@@ -1,56 +1,58 @@
 #include "User.h"
 
-int user_isalnum(int aCharacter)
-{
-  return -1;
-}
 
-int user_isalpha(int aCharacter)
-{
-  return -1;
-}
 
-int user_islower(int aCharacter)
+int user_isspace(int aCharacter)
 {
-  return -1;
-}
-
-int user_isupper(int aCharacter)
-{
-  return -1;
+	return -1;
 }
 
 int user_isdigit(int aCharacter)
 {
-  return -1;
+	return -1;
 }
 
 int user_isxdigit(int aCharacter)
 {
-  return -1;
+	return -1;
+}
+
+int user_islower(int aCharacter)
+{
+	return -1;
+}
+
+int user_isupper(int aCharacter)
+{
+	return -1;
+}
+
+int user_isalpha(int aCharacter)
+{
+	return -1;
+}
+
+int user_isalnum(int aCharacter)
+{
+	return -1;
 }
 
 int user_iscntrl(int aCharacter)
 {
-  return -1;
+	return -1;
 }
 
 int user_isgraph(int aCharacter)
 {
-  return -1;
-}
-
-int user_isspace(int aCharacter)
-{
-  return -1;
+	return -1;
 }
 
 int user_isprint(int aCharacter)
 {
-  return -1;
+	return -1;
 }
 
 int user_ispunct(int aCharacter)
 {
-  return -1;
+	return -1;
 }
--- User.h
+++ User.h
@@ -3,17 +3,8 @@
 #ifndef USER_H
 #define USER_H
 
-//checks if a character is alphanumeric
-int user_isalnum(int aCharacter);
-
-//checks if a character is alphabetic 
-int user_isalpha(int aCharacter);
-
-//checks if a character is lowercase 
-int user_islower(int aCharacter);
-
-//checks if a character is an uppercase character 
-int user_isupper(int aCharacter);
+//checks if a character is a space character 
+int user_isspace(int aCharacter);
 
 //checks if a character is a digit 
 int user_isdigit(int aCharacter);
@@ -21,14 +12,23 @@ int user_isdigit(int aCharacter);
 //checks if a character is a hexadecimal character 
 int user_isxdigit(int aCharacter);
 
+//checks if a character is lowercase 
+int user_islower(int aCharacter);
+
+//checks if a character is an uppercase character 
+int user_isupper(int aCharacter);
+
+//checks if a character is alphabetic 
+int user_isalpha(int aCharacter);
+
+//checks if a character is alphanumeric
+int user_isalnum(int aCharacter);
+
 //checks if a character is a control character 
 int user_iscntrl(int aCharacter);
 
 //checks if a character is a graphical character 
 int user_isgraph(int aCharacter);
-
-//checks if a character is a space character 
-int user_isspace(int aCharacter);
 
 //checks if a character is a printing character 
 int user_isprint(int aCharacter);
